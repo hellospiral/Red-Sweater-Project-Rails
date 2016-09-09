03cbbc63c14c7028f40a40b329ce2b021be0de37
Julia Dickey <julia.dickey@gmail.com>
2016-09-09 16:29:24 -0700

    Pass spec to update students

--- Red_Sweater/spec/features/student_pages_spec.rb
+++ Red_Sweater/spec/features/student_pages_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
-describe "adding a student" do
+describe "student" do
   it "adds a new student" do
     student = Student.create(name: "Emmanuel Andrea", age: 14, bio: "I want to go do school", dream: "pilot", sponsored: false, image: "emmanuelandrea", gender: "boy")
     visit students_path
     expect(page).to have_content "Emmanuel Andrea"
+  end
+  it "updates an existing student" do
+    student = Student.create(name: "Emmanuel Andrea", age: 14, bio: "I want to go do school", dream: "pilot", sponsored: false, image: "emmanuelandrea", gender: "boy")
+    visit admin_index_path
+    click_link "Update Emmanuel Andrea"
+    fill_in 'Name', :with => 'Eman Andre'
+    click_button 'Update Student'
+    expect(page).to have_content 'Eman Andre'
   end
 end
