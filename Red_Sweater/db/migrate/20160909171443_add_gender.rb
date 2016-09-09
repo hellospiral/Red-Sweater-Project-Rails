4cdaf45030274906712994b81adfba2ecc8bca25
Julia Dickey <julia.dickey@gmail.com>
2016-09-09 11:03:21 -0700

    Update styling, and add images to students

---
+++ Red_Sweater/db/migrate/20160909171443_add_gender.rb
@@ -0,0 +1,5 @@
+class AddGender < ActiveRecord::Migration[5.0]
+  def change
+    add_column :students, :gender, :string
+  end
+end
--- Red_Sweater/db/seeds.rb
+++ Red_Sweater/db/seeds.rb
@@ -1,12 +1,12 @@
 
-Student.create(name: "Mwandawa Ramadhani", age: 15, bio: "I want to go do school", dream: "teacher", sponsored: false, image: "mwadawaramadhani")
+Student.create(name: "Mwandawa Ramadhani", age: 15, bio: "I want to go do school", dream: "teacher", sponsored: false, image: "mwadawaramadhani", gender: "girl")
 
-Student.create(name: "Mwajabu Kilango", age: 13, bio: "I want to go do school", dream: "nurse", sponsored: false, image: "mwajabukilango")
+Student.create(name: "Mwajabu Kilango", age: 13, bio: "I want to go do school", dream: "nurse", sponsored: false, image: "mwajabukilango", gender: "girl")
 
-Student.create(name: "Shedrack Japhet", age: 16, bio: "I want to go do school", dream: "teacher", sponsored: false, image: "shedrackjaphet")
+Student.create(name: "Shedrack Japhet", age: 16, bio: "I want to go do school", dream: "teacher", sponsored: false, image: "shedrackjaphet", gender: "boy")
 
-Student.create(name: "Zacharia Yakoni", age: 17, bio: "I want to go do school", dream: "tour guide", sponsored: false, image: "zachariayakoni")
+Student.create(name: "Zacharia Yakoni", age: 17, bio: "I want to go do school", dream: "tour guide", sponsored: false, image: "zachariayakoni", gender: "boy")
 
-Student.create(name: "Raphael Joeli", age: 16, bio: "I want to go do school", dream: "doctor", sponsored: false, image: "raphaeljoeli")
+Student.create(name: "Raphael Joeli", age: 16, bio: "I want to go do school", dream: "doctor", sponsored: false, image: "raphaeljoeli", gender: "boy")
 
-Student.create(name: "Emmanuel Andrea", age: 14, bio: "I want to go do school", dream: "pilot", sponsored: false, image: "emmanuelandrea")
+Student.create(name: "Emmanuel Andrea", age: 14, bio: "I want to go do school", dream: "pilot", sponsored: false, image: "emmanuelandrea", gender: "boy")
