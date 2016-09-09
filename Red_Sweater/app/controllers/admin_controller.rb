8ecec411288681caa8508b0387e1d68925e284a1
Julia Dickey <julia.dickey@gmail.com>
2016-09-09 15:59:39 -0700

    Allow admin to create, list, update and delete events, and students

--- Red_Sweater/app/controllers/admin_controller.rb
+++ Red_Sweater/app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   def index
     @students = Student.all
+    @events = Event.all
   end
 end
--- Red_Sweater/app/controllers/events_controller.rb
+++ Red_Sweater/app/controllers/events_controller.rb
@@ -11,11 +11,30 @@ class EventsController < ApplicationController
       redirect_to events_path
     else
       render :new
+    end
   end
-end
+  def edit
+    @event = Event.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to :root
+  end
 
   private
   def event_params
-    params.require(:event).permit(:name, :date, :time, :venu, :address, :cost, :details)
+    params.require(:event).permit(:name, :date, :time, :venu, :address, :cost, :details, :image)
   end
 end
