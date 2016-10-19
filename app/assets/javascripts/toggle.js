f0c393b2907a4f092adf61712e00ad811f099227
Julia Dickey <julia.dickey@gmail.com>
2016-10-19 11:47:44 -0700

    fix delay in toggle for Our Team page. Add all team members, and Board Member section

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -1,17 +1,1 @@
-$(document).ready(function() {
 
-  $('#mobile-menu-icon').click(function() {
-    $('.mobile-menu').toggle(700);
-  });
-
-  $('#ashley-arrow-down').click(function() {
-    $('#ashley-details').toggle(700);
-    $('#ashley-arrow-down').hide();
-    $('#ashley-arrow-up').show();
-  });
-  $('#ashley-arrow-up').click(function() {
-    $('#ashley-details').toggle(700);
-    $('#ashley-arrow-down').show();
-    $('#ashley-arrow-up').hide();
-  });
-});
