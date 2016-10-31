9ce59740fda109af402f4bf102e000d71aff8fc1
Julia Dickey <julia.dickey@gmail.com>
2016-10-31 11:02:41 -0700

    Add Red Rhino Tours page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
   validates :name, :presence => true
   validates :name, :length => { maximum: 30 }
+  validates :image, :presence => true
+
+  default_scope {order('created_at DESC')}
+
 end
