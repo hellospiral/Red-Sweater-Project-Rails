3b479f6d138361510fc7287630d24cce579b911f
Julia Dickey <julia.dickey@gmail.com>
2016-11-01 10:00:11 -0700

    rollback extra migration

--- db/schema.rb
+++ db/schema.rb
@@ -125,7 +125,6 @@ ActiveRecord::Schema.define(version: 20161005235712) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "blog",                   default: false
-    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
