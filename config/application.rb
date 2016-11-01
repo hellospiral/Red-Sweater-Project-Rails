e1fd80a47b4ff386ded6dd442d29efe08b4970ce
Julia Dickey <julia.dickey@gmail.com>
2016-11-01 09:32:37 -0700

    Comment out rails in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails/all"
+# require "rails/all"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
