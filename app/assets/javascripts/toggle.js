8fd4377b1fc0190845696f336f9532b29adcdf87
Julia Dickey <julia.dickey@gmail.com>
2016-12-13 21:27:26 -0800

    Add static image on homepage, update cover image, and animate in our focus

--- app/assets/javascripts/toggle.js
+++ app/assets/javascripts/toggle.js
@@ -1,1 +1,13 @@
 
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+
+  if(wScroll > $('.focus-items').offset().top - ($(window).height()/1.2)) {
+
+    $('.focus-items div').each(function(i){
+      setTimeout(function(){
+        $('.focus-items div').eq(i).addClass('is-showing');
+      }, 150 * (i + 1));
+      });
+  }
+});
