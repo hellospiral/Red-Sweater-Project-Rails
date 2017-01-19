6150c3acd5ddf8bea6a277b5f3aa0c38a5bf1fda
Julia Dickey <julia.dickey@gmail.com>
2017-01-19 09:46:56 -0800

    Refactoring SCSS to organized SASS, adding content from Ashley

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,39 +1,25 @@
 class ChargesController < ApplicationController
 
-def new
-end
-
-def create
-  @amount = params[:amount]
-
-  @amount = @amount.gsub('$', '').gsub(',', '')
-
-  begin
-    @amount = Float(@amount).round(2)
-  rescue
-    flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
-    redirect_to new_charge_path
-    return
+  def new
   end
 
-  @amount = (@amount * 100).to_i # Must be an integer!
+  def create
+    @amount = 15000
 
-  if @amount < 500
-    flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
-    redirect_to new_charge_path
-    return
-  end
+    customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+    )
 
-  Stripe::Charge.create(
-    :amount => @amount,
-    :currency => 'usd',
-    :source => params[:stripeToken],
-    :description => 'Custom donation'
-  )
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :currency => 'usd',
+      :description => 'Monthly sponsorship'
+    )
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-end
-
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to new_charge_path
+    end
 end
