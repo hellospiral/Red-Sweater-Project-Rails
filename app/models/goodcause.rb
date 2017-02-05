d1e67b55f90341e0ae53d9986e007470c326edc7
Julia Dickey <Julia@Julias-MacBook-Pro.local>
2017-02-05 15:10:30 -0800

    Fix homepage responsiveness so 3 columns break correctly, and single columns have correct spacing

--- app/models/goodcause.rb
+++ app/models/goodcause.rb
@@ -4,4 +4,6 @@ class Goodcause < ApplicationRecord
   has_attached_file :image, :styles => { :medium => "300x300#"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  default_scope {order('cost ASC')}
+
 end
