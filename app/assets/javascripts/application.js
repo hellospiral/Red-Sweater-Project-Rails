0714f702160e43f3ab7aaa2845c0301be5849c93
Julia Dickey <Julia@Julias-MacBook-Pro.local>
2017-02-19 10:34:39 -0800

    Add jquery cdn link and create a modal that pops up with the Privacy Policy when a user clicks the privacy link in the footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-// = require bootstrap-sprockets
+//= require bootstrap
 //= require underscore
 //= require gmaps/google
 //= require ckeditor/init
-// = require_tree .
+//= require_tree .
